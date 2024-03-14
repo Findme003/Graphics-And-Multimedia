45d79ed2c0b09816d48d953c0cb02d56cde26f6a
Findme003 <163382202+Findme003@users.noreply.github.com>
2024-03-14 11:54:09 +0530

    Create prog3.c

---
+++ prog3.c
@@ -0,0 +1,38 @@
+
+#include<graphics.h>
+#include<conio.h>
+#include<stdio.h>
+#include<math.h>
+int i,steps,dx,dy,xa,xb,ya,yb,d,m;
+int xinc,yinc,x,y;
+void main()
+{
+d=DETECT;
+initgraph(&d,&m,"c:\\turboc3\\bgi");
+printf("\n Enter values x1,x2,y1,y2 \n");
+scanf("%d %d %d %d",&xa,&xb,&ya,&yb);
+outtextxy(150,150,"DDA line drawing algorithm \n");
+dx=xb-xa;
+dy=yb-ya;
+if(abs(dx)>abs(dy))
+{
+steps=abs(dx);
+}
+Else
+{
+steps=abs(dy);
+xinc=dx/steps;
+yinc=dy/steps;
+x=xa;
+y=ya;
+putpixel((int)x,(int)y,15);
+}
+for(i=1;i<=steps;i++)
+{
+x=x+xinc;
+y=y+yinc;
+putpixel((int)x,(int)y,15);
+}
+getch();
+}
+
